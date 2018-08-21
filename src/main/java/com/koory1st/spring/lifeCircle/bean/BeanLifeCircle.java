489b76a6e0ca02950b07d0cf5bdb7b4631c6ead2
koory1st <32436334@qq.com>
2018-08-21 19:03:23 +0800

    接口覆盖方法，xml配置bean，全局xml配置全部都配置上，按顺序执行接口覆盖方法及xml配置bean。

--- src/main/java/com/koory1st/spring/lifeCircle/bean/BeanLifeCircle.java
+++ src/main/java/com/koory1st/spring/lifeCircle/bean/BeanLifeCircle.java
@@ -3,24 +3,24 @@ package com.koory1st.spring.lifeCircle.bean;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class BeanLifeCircle {
-//    @Override
-//    public void destroy() throws Exception {
-//        System.out.println("bean destroy");
-//    }
-//
-//    @Override
-//    public void afterPropertiesSet() throws Exception {
-//        System.out.println("bean init");
-//
-//    }
-//    private void start() {
-//        System.out.println("bean start");
-//    }
-//
-//    private void stop() {
-//        System.out.println("bean stop");
-//    }
+public class BeanLifeCircle implements InitializingBean, DisposableBean {
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("bean destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("bean init");
+
+    }
+    private void start() {
+        System.out.println("bean start");
+    }
+
+    private void stop() {
+        System.out.println("bean stop");
+    }
 
 
     private void defaultInitMethod() {
