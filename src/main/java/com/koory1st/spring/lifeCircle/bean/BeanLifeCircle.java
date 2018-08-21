6483a6a75ea2bb86faa5e2550f1b244b190b25bd
koory1st <32436334@qq.com>
2018-08-21 18:33:48 +0800

    实现接口覆盖方式的生命周期

--- src/main/java/com/koory1st/spring/lifeCircle/bean/BeanLifeCircle.java
+++ src/main/java/com/koory1st/spring/lifeCircle/bean/BeanLifeCircle.java
@@ -1,11 +1,24 @@
 package com.koory1st.spring.lifeCircle.bean;
 
-public class BeanLifeCircle {
-    private void start() {
-        System.out.println("bean start");
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class BeanLifeCircle implements InitializingBean, DisposableBean {
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("bean destroy");
     }
 
-    private void stop() {
-        System.out.println("bean stop");
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("bean init");
+
     }
+//    private void start() {
+//        System.out.println("bean start");
+//    }
+//
+//    private void stop() {
+//        System.out.println("bean stop");
+//    }
 }
